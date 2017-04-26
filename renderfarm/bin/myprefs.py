0f61ee3b6bebb34700a97999602ed46c3bd0b367
mgidney <mgidney@gmail.com>
2017-04-26 13:18:12 +1000

    utilities for name

---
+++ renderfarm/bin/myprefs.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+
+# ##############################################################
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+sh = logging.StreamHandler()
+sh.setLevel(logging.INFO)
+formatter = logging.Formatter('%(levelname)5.5s \t%(name)s \t%(message)s')
+sh.setFormatter(formatter)
+logger.addHandler(sh)
+# ##############################################################
+
+import argparse
+import os
+import sys
+
+from renderfarm.dabtractor.factories import shotgun_factory as sgt
+
+def getuserprefs():
+    try:
+        p=sgt.Person()
+
+    except Exception,err:
+        logger.critical("Cant talk to shotgun properly {}".format(err))
+    else:
+        if os.path.exists(p.user_prefs):
+            return p.user_prefs
+        else:
+            sys.exit(1)
+
+
+# #####################################################################################################
+if __name__ == '__main__':
+    print getuserprefs()
